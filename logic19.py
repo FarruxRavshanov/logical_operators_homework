4bd830a203546d0f9f0dcb432b2c53cb1ca923ef
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-16 10:26:36 +0300

    task19

--- logic19.py
+++ logic19.py
@@ -1,11 +1,15 @@
 def main(x):
     """
-    Given an integer x, return true if x is palindrome integer.
+    Given an integer x, return true if x is palindrome integer. 
     An integer is a palindrome when it reads the same backward as forward.
-
     Args:
         x(int): parameter x
     Returns:
         bool: answer
     """
-    return
+    s = str(x)
+    z = int(s[::-1])
+    d = x == z
+    return d
+
+print(main(777))
