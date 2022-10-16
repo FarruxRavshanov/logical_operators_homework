935fedc7ae740a5c18f6daacf6f58fd73bf91846
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-16 10:03:05 +0300

    task18

--- logic18.py
+++ logic18.py
@@ -5,4 +5,12 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x1 = a % 10
+    x2 = a // 10 % 10
+    x3 = a // 100 % 10
+    x4 = a // 1000 % 10
+    x5 = a // 10000
+    s = x1 > x2 > x3 > x4 > x5
+    return s
+
+print(main(12345))
