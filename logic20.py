7806f26c56d571afcf3999734dd5bf21e6aa3c74
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-20 18:31:11 +0300

    task20

--- logic20.py
+++ logic20.py
@@ -8,5 +8,26 @@ def main(n):
     Returns:
         bool: answer
     """
+    count0 = 0
+    count1 = 0
 
-    return
+    x1 = n % 10
+    count0 += x1 == 0
+    count1 += x1 == 1
+    n //= 10
+
+    x2 = n // 10 % 10
+    count0 += x2 == 0
+    count1 += x2 == 1
+
+    x3 = n // 100 % 10
+    count0 += x3 == 0
+    count1 += x3 == 1
+
+    x4 = n // 1000
+    count0 += x4 == 0
+    count1 += x4 == 1
+
+    return count1 > count0
+
+print(main(1011))
