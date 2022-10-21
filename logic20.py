970ead81bc8a74c587649de78c7220ce602d9430
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-21 18:13:08 +0300

    task20

--- logic20.py
+++ logic20.py
@@ -12,35 +12,22 @@ def main(n):
     count1 = 0
 
     x1 = n % 10
-    if x1 == 1:
-        count1 += 1
-    else:
-        count0 += 1
+    count0 += x1 == 0
+    count1 += x1 == 1
+    n //= 10
 
-    x2 = n // 10 % 10
-    if x2 == 1:
-        count1 += 1
-    else:
-        count0 += 1
+    x2 = n % 10
+    count0 += x2 == 0
+    count1 += x2 == 1
+    n //= 10
 
-    x3 = n // 100 % 10
-    if x3 == 1:
-        count1 += 1
-    else:
-        count0 += 1
+    x3 = n % 10
+    count0 += x3 == 0
+    count1 += x3 == 1
+    n //= 10
 
-    x4 = n // 1000 % 10
-    if x4 == 1:
-        count1 += 1
-    else:
-        count0 += 1
-
-    x5 = n // 10000
-    if x5 == 1:
-        count1 += 1
-    else:
-        count0 += 1
+    x4 = n % 10
+    count0 += x4 == 0
+    count1 += x4 == 1
 
     return count1 > count0
-
-print(main(10001))
